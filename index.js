131a59fc02dfec69953b773702e4b9382c586b2f
Gabriel Lovera <gabrielloveraa@gmail.com>
2022-04-25 12:51:19 -0300

    fix(app): fix server

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import 'dotenv/config'
 
 const app = express()
 
-app.listen(process.env.PORT || 3000, () => {
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
