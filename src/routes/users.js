54007f9b8896fa85ee917a1c7ea06c30237d5718
Gabriel Lovera <gabrielloveraa@gmail.com>
2022-04-27 11:57:11 -0300

    feat: add user entity

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,15 @@
+import {
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} from '../controllers/users.controller.js'
 import express from 'express'
 const router = express.Router()
 
-router.get('/', (req, res) => {})
+router.post('/', createUser)
+router.get('/:id', getUser)
+router.put('/:id', updateUser)
+router.delete('/:id', deleteUser)
 
 export default router
